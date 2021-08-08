d4805a65596bb4e27a3b41e54437c8cf0a965593
AriesPL <blackhorns1989@gmail.com>
2021-08-08 21:25:26 +0300

    Добавьте файлы проекта.

---
+++ HomeWork1.1/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HomeWork1._1
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			//Написать программу анкета.
+
+			Console.WriteLine("Enter you name:");
+			string name = Convert.ToString(Console.ReadLine());
+
+			Console.WriteLine("Enter second name:");
+			string secname = Convert.ToString(Console.ReadLine());
+
+			Console.WriteLine("Enter years:");
+			int year = Convert.ToInt32(Console.ReadLine());
+
+			int born = DateTime.Now.Year - year;
+
+			Console.WriteLine("Enter height:");
+			int height = Convert.ToInt32(Console.ReadLine());
+
+			Console.WriteLine("Enter mass");
+			int mass = Convert.ToInt32(Console.ReadLine());
+
+			Console.WriteLine("Hi,"+ secname + " " + name + ". Nice to meet to you."); // Вывод склейкой.
+			Console.WriteLine($"Now {year} years. You born in {born}."); // Вывод  со знаком $.
+			Console.WriteLine("Your height {0} and mass {1}. Excellent result, you impressed me.",height,mass); // Форматированый вывод
+
+			Console.ReadLine();
+		}
+
+		
+	}
+}
---
+++ HomeWork1.2/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HomeWork1._2
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			//Расчитать массу человека по веденым росту и весу.
+
+			Console.WriteLine("Enter height:");
+			float height = Convert.ToInt32(Console.ReadLine());
+
+			Console.WriteLine("Enter mass");
+			float mass = Convert.ToInt32(Console.ReadLine());
+
+			float indexMass = ((height * height) / 100)/ mass; 
+
+			Console.WriteLine("{0,0:f2}. You surprised me.", indexMass); 
+			Console.ReadLine();
+		}
+	}
+}
