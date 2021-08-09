f11054846b188c7e51e1924d4d2b4aea6e0dcf2d
AriesPL <blackhorns1989@gmail.com>
2021-08-09 16:00:15 +0300

    Complete HomeWork 1/3

--- HomeWork1.3/Program.cs
+++ HomeWork1.3/Program.cs
@@ -10,6 +10,50 @@ namespace HomeWork1._3
 	{
 		static void Main(string[] args)
 		{
+			//Программа вычисления растояния между точками.
+			ConWrite("Enter point x1:");
+			int x1 = ConRead();
+
+			ConWrite("Enter point x2:");
+			int x2 = ConRead();
+
+			ConWrite("Enter point y1:");
+			int y1 = ConRead();
+
+			ConWrite("Enter point y2:");
+			int y2 = ConRead();
+
+			double lenght = GetLenght(x1, x2, y1, y2);
+
+			ConWrite("Distance between points {0,0:f2}", lenght);
+
+			Console.ReadLine();
 		}
+
+		private static int ConRead()
+		{
+			return Convert.ToInt32(Console.ReadLine());
+		}
+
+		private static double GetLenght(int x1, int x2, int y1, int y2) // вывести вычисление в отдельный метод.
+		{
+			return Math.Sqrt(Math.Pow(x2 - x1, 2) + Math.Pow(y2 - y1, 2));
+		}
+
+		private static void ConWrite(string v, double lenght)// Сделать перегрузку методов.
+		{
+			Console.WriteLine(v, lenght);
+			
+		}
+
+		private static void ConWrite(string v)//Сделать перегрузку методов.
+		{
+			Console.WriteLine(v);
+			
+		}
+		
+		
+			
+		
 	}
 }
