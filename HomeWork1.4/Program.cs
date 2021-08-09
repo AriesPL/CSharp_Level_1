f3f8acfcee3156ce8ed48a3c843163101f95aa5e
AriesPL <blackhorns1989@gmail.com>
2021-08-09 16:00:55 +0300

    Complete HomeWork 1/4

--- HomeWork1.4/Program.cs
+++ HomeWork1.4/Program.cs
@@ -10,6 +10,40 @@ namespace HomeWork1._4
 	{
 		static void Main(string[] args)
 		{
+			//обмен значениями двух переменных с использованием третьей и без.
+
+			int x = 2;
+			int y = 8;
+			int z = 0;
+			Print(x, y, z);
+
+			Change(x, y, z);
+
+			Change(x, y);
+
+			Console.ReadLine();
+		}
+
+		private static void Print(int x, int y, int z)
+		{
+			
+			Console.WriteLine("x = {0}\ny = {1}\nz = {2}", x, y,z);
+		}
+		private static void Change(int x, int y, int z)
+		{
+			z = x;
+			x = y;
+			y = z;
+
+			Console.WriteLine("\nx = {0}\ny = {1}", x, y);
+		}
+		private static void Change(int x, int y)
+		{
+			x = x + y;
+			y = x - y;
+			x = x - y;
+
+			Console.WriteLine("\nx = {0}\ny = {1}", x, y);
 		}
 	}
 }
