bb9f7745f2eb36fe32a18b346aa6650dc9a96cd2
AriesPL <blackhorns1989@gmail.com>
2021-08-10 15:49:44 +0300

    Complete HomeWork 1/5

---
+++ HomeWork1.5/Print.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HomeWork1._5
+{
+	public class Print
+	{
+		public void Write(string v)
+		{
+			Console.WriteLine(v);
+		}
+	}
+}
--- HomeWork1.5/Program.cs
+++ HomeWork1.5/Program.cs
@@ -4,12 +4,27 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+
 namespace HomeWork1._5
 {
 	class Program
 	{
 		static void Main(string[] args)
 		{
+			//Вывод Имя,Фамилию и город. Организовать вывод в центре консоли.
+			Print print = new Print();// Создать свой класс Print
+			Pause pause = new Pause();// Сщздать свой класс Pause
+			
+			int top, left;
+			string info = "Sergeev Alexey. Tomsk city";
+			
+			left = Console.WindowWidth / 2;
+			top = Console.WindowHeight / 2;
+			
+			Console.SetCursorPosition(left - info.Length/2, top);
+			print.Write(info);
+
+			pause.Stop();
 		}
 	}
 }
