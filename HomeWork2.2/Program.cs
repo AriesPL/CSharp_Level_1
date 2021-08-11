79b12441189679e7466d7299afba4784065311c8
AriesPL <blackhorns1989@gmail.com>
2021-08-11 07:26:51 +0300

    Complete HomeWork 2/2

---
+++ HomeWork2.2/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using HomeWork1._5;
+
+namespace HomeWork2._2
+{
+	class Program
+	{
+		// Написать метод подсчета количества цифр.
+		static void Main(string[] args)
+		{
+			
+			LenghtValue();
+
+			Pause.Stop();
+		}
+
+		private static void LenghtValue()
+		{
+			Random rnd = new Random();
+
+			int x = rnd.Next(1000, 1000000000);
+			string longValue = Convert.ToString(x);
+
+			Print.Write("The length of '{0}' is '{1}'", longValue, longValue.Length);
+		}
+	}
+}
