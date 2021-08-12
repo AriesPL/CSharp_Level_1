6c8e95ad400c8cef51ae05699006a2b80595030e
AriesPL <blackhorns1989@gmail.com>
2021-08-12 08:55:26 +0300

    Complete HomeWork2/4

---
+++ HomeWork2.4/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HomeWork2._4
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			//Метод проверки логина и пароля. log = root ; pass = 12345
+
+			LogPas();
+
+			Console.ReadLine();
+
+		}
+
+		private static void LogPas()
+		{
+			string log, pass;
+			int count = 0;
+			do
+			{
+					Console.WriteLine("Enter Login:");
+					log = Console.ReadLine();
+
+					Console.WriteLine("Enter Password:");
+					pass = Console.ReadLine();
+
+					count++;
+			}
+			while (log != "root" && pass != "12345" && count != 3);
+
+			if (log == "root" && pass == "12345")
+			{
+				Console.WriteLine("You entered");
+			}
+			else
+				Console.WriteLine("you used {0} tries", count);
+
+
+		}
+	}
+}
