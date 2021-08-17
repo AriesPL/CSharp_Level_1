df9ff399d9f1212a7399d8b3f8d8042ca7a8dc53
AriesPL <blackhorns1989@gmail.com>
2021-08-17 17:18:53 +0300

    Complete HomeWork 3/2

---
+++ HomeWork3.2/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HomeWork3._2
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			Console.WriteLine("Введите число:");
+			int userInt;
+			int sumUser =0;
+			string strResult = "";
+			do
+			{
+				bool flag;
+				do
+				{
+					flag = Int32.TryParse(Console.ReadLine(), out userInt);
+				}
+				while (!flag);
+				if (userInt % 2 != 0 && userInt > 0)
+				{
+					sumUser += userInt;
+					strResult += Convert.ToString(userInt) + ", ";
+				}
+			}
+			while (userInt != 0);
+
+			Console.WriteLine(strResult);
+			Console.WriteLine("Сумма нечетных положительных чисел {0}",sumUser);
+
+			Console.ReadLine();
+		}
+	}
+}
