02b415b10922c3c3b934567e3baa0827121de568
AriesPL <blackhorns1989@gmail.com>
2021-08-26 11:12:07 +0300

    Complete HomeWork 4/1

--- HomeWork4.1/Program.cs
+++ HomeWork4.1/Program.cs
@@ -6,35 +6,36 @@ using System.Threading.Tasks;
 
 namespace HomeWork4._1
 {
+
 	// написать класс массивов, который принимает рандомные числа от -10000 до 10000 и ищет среди них пары делящиеся на 3
 	class MyMas
 	{
-		int[] a;
 		
+		int[] a;
 
-		public MyMas(int size) 
+		public MyMas(int size)
 		{
 			a = new int[size];
 		}
-		public MyMas(int size,int min,int max) //перегрузка с 3 паранитрами.
+		public MyMas(int size, int min, int max) //перегрузка с 3 паранитрами.
 		{
 			a = new int[size];
 			Random rnd = new Random();
-			
+
 			for (int i = 0; i < size; i++)
 			{
-				a[i] =rnd.Next(min, max + 1);
+				a[i] = rnd.Next(min, max + 1);
 			}
-			
+
 		}
-		
-		
+
+
 		public int this[int index]// делаем метод для получения элемента массива через индексатор
 		{
 			get { return a[index]; }
 			set { a[index] = value; }
 		}
-		
+
 		public int Lenght //метод длины массива
 		{
 			get { return a.Length; }
@@ -42,7 +43,7 @@ namespace HomeWork4._1
 		public int Divisibility3() //метод сравнения массивов и поиску пар делящихся на 3
 		{
 			int count = 0;
-			for (int i = 0; i < a.Length-1; i++)
+			for (int i = 0; i < a.Length - 1; i++)
 			{
 				if (a[i] % 3 == 0 && a[i + 1] % 3 == 0 && a[i] != 0)
 				{
@@ -53,7 +54,7 @@ namespace HomeWork4._1
 			Console.WriteLine("Количество пар: " + count);
 			return count;
 		}
-		public override string ToString() 
+		public override string ToString()
 		{
 			string s = "";
 			foreach (int element in a)
@@ -62,20 +63,27 @@ namespace HomeWork4._1
 			return s;
 		}
 		
+		//Реализовать задание 1 ввиде статического класса.
+
+
+
 	}
 	class Program
 	{
-		
+
 		static void Main(string[] args)
 		{
-			MyMas myMas = new MyMas(100,-10000,10000);
+			
+			MyMas myMas = new MyMas(100, -10000, 10000);
 
 			Console.WriteLine($"Создан массив чисел :\n {myMas} \n\nКакие массивы парные:");
 
 			myMas.Divisibility3();
 			
+
+
 			Console.ReadLine();
-			
 		}
 	}
 }
+
